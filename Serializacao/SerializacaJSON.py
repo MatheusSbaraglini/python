c94d35dd3011c6bc304dab0ebef0cf5bbccfeb0c
Matheus Sbaraglini <matheus.sbaraglini@hotmail.com>
2018-03-19 23:25:26 -0300

    add aula serializacao

---
+++ Serializacao/SerializacaJSON.py
@@ -0,0 +1,14 @@
+import json
+
+class Contato:
+
+    def __init__(self, primeiro_nome, sobrenome):
+        self.primeiro_nome = primeiro_nome
+        self.sobrenome = sobrenome
+
+    @property
+    def nome_completo(self):
+        return ('{} {}'.format(self.primeiro_nome, self.sobrenome))
+
+c = Contato('Matheus', 'Sbaraglini')
+print(json.dumps(c.__dict__))
