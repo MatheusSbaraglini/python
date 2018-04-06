e95df8667e12383405f137f85e439e4ce95d356d
Matheus Sbaraglini <matheus.sbaraglini@hotmail.com>
2018-04-05 23:44:17 -0300

    testes json

--- Serializacao/serializaca_json.py
+++ Serializacao/serializaca_json.py
@@ -1,14 +1,23 @@
 import json
+import pickle
+
 
 class Contato:
 
-    def __init__(self, primeiro_nome, sobrenome):
+    def __init__(self, primeiro_nome, sobrenome, endereco):
         self.primeiro_nome = primeiro_nome
         self.sobrenome = sobrenome
+        self.endereco = endereco
 
     @property
     def nome_completo(self):
         return ('{} {}'.format(self.primeiro_nome, self.sobrenome))
 
-c = Contato('Matheus', 'Sbaraglini')
-print(json.dumps(c.__dict__))
+# c = Contato('Matheus', 'Sbaraglini', 'rua sp')
+# print(json.dumps(c.__dict__))
+
+contato = Contato('Matheus', 'Sbaraglini', 'rua floripa')
+# s_json = json.dumps(contato.__dict__)
+s_json = "{'teste': 'valor_teste', 'teste1': 'valor_teste1'}"
+contato.__dict__ = dict(eval(s_json))
+print(contato.teste)
