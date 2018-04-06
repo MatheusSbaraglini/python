4e7f19d8ef5b528eb1991588d796a7a5aa4fee6b
Matheus Sbaraglini <matheus.sbaraglini@hotmail.com>
2018-04-05 23:52:09 -0300

    alterando consultas

--- database/database.py
+++ database/database.py
@@ -20,6 +20,7 @@ class BancoDeDados:
             CREATE TABLE IF NOT EXISTS clientes(
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 nome TEXT NOT NULL,
+                senha VARCHAR(20) NOT NULL,
                 cpf VARCHAR(11) UNIQUE NOT NULL,
                 email TEXT NOT NULL
             );
@@ -27,14 +28,15 @@ class BancoDeDados:
         except AttributeError:
             print('Faça a conexão do banco antes de criar as tabelas.')
 
-    def inserir_cliente(self, nome, cpf, email):
+    def inserir_cliente(self, nome, senha, cpf, email):
         try:
             cursor = self.conexao.cursor()
 
             try:
                 cursor.execute("""
-                    INSERT INTO clientes(nome, cpf, email) VALUES(?,?,?)
-                """, (nome, cpf, email))
+                    INSERT INTO clientes(nome, senha, cpf, email) VALUES(?,?,
+                                                                         ?,?)
+                """, (nome, senha, cpf, email))
             except sqlite3.IntegrityError:
                 print('O cpf %s já existe' % cpf)
 
@@ -45,9 +47,15 @@ class BancoDeDados:
     def buscar_cliente(self, cpf):
         cursor = self.conexao.cursor()
 
-        cursor.execute("""SELECT * FROM clientes;""")
+        cursor.execute("SELECT * FROM clientes WHERE cpf=?", [(cpf)])
 
-        for linha in cursor.fetchall():
+        cliente = cursor.fetchone()
+
+        if cliente:
+            print('cliente %s encontrado' % cliente[1])
+        else:
+            print('cliente não encontrado')
+        """for linha in cursor.fetchall():
             if linha[2] == cpf:
                 print('Cliente %s encontrado.' % linha[1])
-                break
+                break"""
