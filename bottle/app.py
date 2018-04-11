ec637bf8f4e83bfa20a3664c8af9438a99b675b9
Matheus Sbaraglini <matheus.sbaraglini@hotmail.com>
2018-04-10 22:24:37 -0300

    add aula simples bottle

--- bottle/app.py
+++ bottle/app.py
@@ -1,5 +1,6 @@
-from bottle import route, run, request
-
+from bottle import get, route, run, request, template, TEMPLATE_PATH
+from bottle import static_file, error
+import os
 
 '''
 @route('/')
@@ -9,15 +10,20 @@ def index(nome='Desconhecido'):
 '''
 
 
+# @get('/<filename:re:.*\.css>')
+@route('/static/css/<filepath:path>')
+def stylesheets(filename):
+    return static_file(filename, root='static/css')
+
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='static/js')
+
 @route('/login')
 def login():
-    return '''
-        <form action="/login" method="post">
-            username: <input name="username" type="text" />
-            password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-    '''
+    TEMPLATE_PATH.insert(0, os.path.abspath(os.path.join(
+                         os.path.dirname(__file__), 'view')))
+    return template('login')
 
 
 def check_login(username, password):
@@ -34,5 +40,11 @@ def acao_login():
     else:
         return '<center><h1> Falha ao realizar o login </h1></center>'
 
+@error(404)
+def error404(error):
+    TEMPLATE_PATH.insert(0, os.path.abspath(os.path.join(
+                         os.path.dirname(__file__), 'view')))
+    return template('pagina404')
+
 if __name__ == '__main__':
-    run(host='localhost', port=8080, debug=True)
+    run(host='localhost', port=8080, debug=False, reloader=True)
